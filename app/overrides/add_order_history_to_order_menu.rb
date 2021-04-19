ec1f24bb63cdf7e9d2bfcddf6a74f8ed1a78c145
Edmundo Perez <ingedmundo@micheladas-MacBook-Pro.local>
2021-04-19 17:38:34 -0500

    Fixes active tab look

--- app/overrides/add_order_history_to_order_menu.rb
+++ app/overrides/add_order_history_to_order_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_order_submenu',
   name: 'order_history_menu',
   insert_bottom: "[data-hook='admin_order_tabs']",
-  text: "<li <%= 'class=\"active\"' if current == 'Order History' %>><%= link_to_with_icon 'icon-shopping-cart', I18n.t('spree.order_history'), versions_admin_order_path(@order) %></li>"
+  text: "<li <%= 'class=active' if current == 'Order History' %>><%= link_to I18n.t('spree.order_history'), versions_admin_order_path(@order) %></li>"
 )
